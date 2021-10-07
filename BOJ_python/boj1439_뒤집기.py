8c839536d3c5f2b02a4f4cea2e20249ff8eaa443
arkdusdyk <arkdusdyk@gmail.com>
2021-10-08 04:06:05 +0900

    boj1439

--- BOJ_python/boj1439_뒤집기.py
+++ BOJ_python/boj1439_뒤집기.py
@@ -3,26 +3,26 @@ import sys
 input = sys.stdin.readline
 
 s = input().rstrip()
-cnt = 0
-for i in range(1, len(s)):	# flip 1s
-	if s[i] == '1':
-		if s[i-1] == '1':
-			continue
+cnt_1 = 0
+cnt_0 = 0
+flag = True
+for i in range(len(s)-1):
+	if s[i] != s[i+1]:
+		if flag:
+			cnt_1 += 1
+			flag = False
 		else:
-			cnt += 1
-cnt1 = 0
-for i in range(1, len(s)):	# flip 0s
-	if s[i] == '0':
-		if s[i-1] == '0':
-			if i == 1:
-				cnt1 += 1
-			continue
-		else:
-			cnt1 += 1
-if cnt1 < cnt:
-	print(cnt1)
+			cnt_0 += 1
+			flag = True
+if flag:	# last check
+	cnt_1 += 1
 else:
-	print(cnt)
+	cnt_0 += 1
+
+if cnt_1 < cnt_0:
+	print(cnt_1)
+else:
+	print(cnt_0)
 '''
 Difficulty : S5
 완전탐색
