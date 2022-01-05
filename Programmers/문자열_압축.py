3ba120b91f62e6243ae99754b6bbc86bb123e2d7
arkdusdyk <arkdusdyk@gmail.com>
2022-01-05 23:28:39 +0900

    Rename

---
+++ Programmers/문자열_압축.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+def solution(s):
+	answer = len(s)
+	for step in range(1,len(s))
+	return answer
+
+s = input().rstrip()
+print(solution(s))
+
+# 2020 카카오 BLIND Recruitment (구현)
