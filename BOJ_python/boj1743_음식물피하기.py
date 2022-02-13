3bb50f5dbcdbda4a6356ba67378e0874bd58d42c
arkdusdyk <arkdusdyk@gmail.com>
2022-02-14 00:40:52 +0900

    21/02/14 boj1743 BFS solved, DFS typeerror

--- BOJ_python/boj1743_음식물피하기.py
+++ BOJ_python/boj1743_음식물피하기.py
@@ -1,9 +1,9 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-sys.getrecursionlimit()
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
-answer = 0
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 n,m,k = map(int, input().split())
 hall = []
 visited = [[False]*m for _ in range(n)]
@@ -13,10 +13,49 @@ for _ in range(k):
 	r, c = map(int, input().split())
 	hall[r-1][c-1] = True
 
-def dfs(i,j, visited):
-	global x
+def bfs(y,x):
+	q = deque()
+	q.append((y,x))
+	visited[y][x] = True
+	tmp = 1
+	while q:
+		y,x = q.popleft()
+		for i in range(4):
+			nx = x+dx[i]
+			ny = y+dy[i]
+			if (0<=nx<m) and (0<=ny<n):
+				if hall[ny][nx] == True and visited[ny][nx] == False:
+					tmp += 1
+					q.append((ny,nx))
+					visited[ny][nx] = True
+	return tmp
+
+answer = 0
+for i in range(n):
+	for j in range(m):
+		if hall[i][j] == True and visited[i][j] == False:
+			res = bfs(i,j)
+			answer = max(res, answer)
+print(answer)
+'''
+import sys
+input = sys.stdin.readline
+sys.getrecursionlimit(10*4)
+dx = [-1,0,1,0]
+dy = [0,-1,0,1]
+n,m,k = map(int, input().split())
+hall = []
+visited = [[False]*m for _ in range(n)]
+for _ in range(n):
+	hall.append([False]*m)
+for _ in range(k):
+	r, c = map(int, input().split())
+	hall[r-1][c-1] = True
+
+def dfs(i,j,visited):
+	global cnt
 	visited[i][j] = True
-	x+=1
+	cnt+=1
 	for k in range(4):
 		nx = j + dx[k]
 		ny = i + dy[k]
@@ -24,16 +63,20 @@ def dfs(i,j, visited):
 			if visited[ny][nx] == False and hall[ny][nx] == True:
 				dfs(ny,nx,visited)
 
+answer = 0
 for i in range(n):
+	cnt = 0
 	for j in range(m):
-		x = 0
 		if visited[i][j] == False and hall[i][j] == True:
 			dfs(i,j,visited)
-			answer = max(x, answer)
+			answer = max(cnt,answer)
 
 print(answer)
 
-'''
+Difficulty : S1
 근처에 있는 것 끼리 뭉친다? => DFS/BFS
-둘 다 풀 수 있을 것 같은데 최근에 BFS로 문제를 풀어서 이번에는 DFS 사용
+1. BFS로 쉽게 해결 가능
+2. DFS로 한 번 풀어보자 => 주석 부분
+	- 런타임에러(RecursionError) : recursionlimit 설정해주면서 재귀깊이 한계를 주어서 해결함.
+	- 런타임에러(TypeError) : 해결 X. 나왔는데 도대체 어딘지..?
 '''
