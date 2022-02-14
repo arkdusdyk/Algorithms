c39fcbccef0c5a408bba87f2fd64e862286dc5c2
arkdusdyk <arkdusdyk@gmail.com>
2022-02-14 17:51:47 +0900

    21/02/14 Fix Codes

--- BOJ_python/DFS&BFS/boj1743_음식물피하기.py
+++ BOJ_python/DFS&BFS/boj1743_음식물피하기.py
@@ -1,7 +1,6 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 n,m,k = map(int, input().split())
@@ -37,46 +36,9 @@ for i in range(n):
 			res = bfs(i,j)
 			answer = max(res, answer)
 print(answer)
+
 '''
-import sys
-input = sys.stdin.readline
-sys.getrecursionlimit(10*4)
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
-n,m,k = map(int, input().split())
-hall = []
-visited = [[False]*m for _ in range(n)]
-for _ in range(n):
-	hall.append([False]*m)
-for _ in range(k):
-	r, c = map(int, input().split())
-	hall[r-1][c-1] = True
-
-def dfs(i,j,visited):
-	global cnt
-	visited[i][j] = True
-	cnt+=1
-	for k in range(4):
-		nx = j + dx[k]
-		ny = i + dy[k]
-		if (0<=ny<n) and (0<=nx<m):
-			if visited[ny][nx] == False and hall[ny][nx] == True:
-				dfs(ny,nx,visited)
-
-answer = 0
-for i in range(n):
-	cnt = 0
-	for j in range(m):
-		if visited[i][j] == False and hall[i][j] == True:
-			dfs(i,j,visited)
-			answer = max(cnt,answer)
-
-print(answer)
-
 Difficulty : S1
 근처에 있는 것 끼리 뭉친다? => DFS/BFS
-1. BFS로 쉽게 해결 가능
-2. DFS로 한 번 풀어보자 => 주석 부분
-	- 런타임에러(RecursionError) : recursionlimit 설정해주면서 재귀깊이 한계를 주어서 해결함.
-	- 런타임에러(TypeError) : 해결 X. 나왔는데 도대체 어딘지..?
+BFS로 쉽게 해결 가능
 '''
